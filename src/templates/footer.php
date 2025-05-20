1abc2da16103b494b3e576dc38ce5589680b4232
ShodiBoy1 <sobirovsh@yahoo.com>
2025-05-20 21:00:48 +0200

    footer

---
+++ src/templates/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <?php echo date('Y'); ?> LOGO. All rights reserved.</p>
+        <div class="social-icons">
+            <a href="https://www.instagram.com/sbrvsh1/"><i class="ri-instagram-fill"></i></a>
+            <a href="https://x.com/"><i class="ri-twitter-x-fill"></i></a>
+            <a href="https://github.com/shodiBoy1"><i class="ri-github-fill"></i></a>
+        </div>
+    </div>
+</footer>
+
+<script src="/assets/js/script.js"></script>
